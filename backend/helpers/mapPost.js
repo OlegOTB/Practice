6209dc61e4d1a96d1b5d7990d9d1d8b0ac5af552
OlegOTB <olegotb@mail.ru>
2024-02-24 09:36:11 +0300

    Дипломный проект версия от 24.02.2024

--- backend/helpers/mapPost.js
+++ backend/helpers/mapPost.js
@@ -8,8 +8,21 @@ module.exports = function (post) {
     imageUrl: post.image,
     content: post.content,
     comments: post.comments.map((comment) =>
-      mongoose.isObjectIdOrHexString(comment) ? comment : mapComment(comment)
+      // mongoose.isObjectIdOrHexString(comment) ? comment : mapComment(comment)
+      isValidObjectId(comment) ? comment : mapComment(comment)
     ),
     publishedAt: post.createdAt,
   };
 };
+
+// Requiring ObjectId from mongoose npm package
+const ObjectId = require("mongoose").Types.ObjectId;
+
+// Validator function
+function isValidObjectId(id) {
+  if (ObjectId.isValid(id)) {
+    if (String(new ObjectId(id)) === String(id)) return true;
+    return false;
+  }
+  return false;
+}
